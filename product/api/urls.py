b0cda61905f1b62f2b036272ab82691186625d3e
ahmed <ahmed.nafies@gmail.com>
2018-10-20 17:04:31 +0300

    Url Patterns for retrive/update/delete of Company and Product are added

--- product/api/urls.py
+++ product/api/urls.py
@@ -1,7 +1,10 @@
 
-from api.views import ReviewRudView
+from api.views import ReviewRudView , CompanyRudView, ProductRudView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^review/id=(?P<pk>\d+)/$', ReviewRudView.as_view(), name='review-rud'),
+    url(r'^company/(?P<pk>\d+)/$', CompanyRudView.as_view(), name='company-rud'),
+    url(r'^product/(?P<pk>\d+)/$', ProductRudView.as_view(), name='product-rud'),
+    url(r'^review/(?P<pk>\d+)/$', ReviewRudView.as_view(), name='review-rud'),
+
 ]
