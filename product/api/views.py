9f27c292b899df806470fcb59911fe27111035e2
ahmed <ahmed.nafies@gmail.com>
2018-10-20 17:04:13 +0300

    Views for retrive/update/delete of Company and Product are added

--- product/api/views.py
+++ product/api/views.py
@@ -1,14 +1,27 @@
 
 from rest_framework import generics
-from api.serializers import ReviewSerializer
+from api.serializers import ReviewSerializer, CompanySerializer, ProductSerializer
 from reviews.models import Company, Product, Review
 
+
+class CompanyRudView(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field = 'pk'
+    serializer_class = CompanySerializer
+
+    def get_queryset(self):
+        return Company.objects.all()
+
+class ProductRudView(generics.RetrieveUpdateDestroyAPIView):
+    lookup_field = 'pk'
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
 
 
 class ReviewRudView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
     serializer_class = ReviewSerializer
-#    queryset = Review.objects.all()
 
     def get_queryset(self):
         return Review.objects.all()
