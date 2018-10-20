ae874b6c55404777cfd9574782773ecb1261c1d8
ahmed <ahmed.nafies@gmail.com>
2018-10-20 21:36:28 +0300

    create / list url patterns for company, product and reviews is added

--- product/api/urls.py
+++ product/api/urls.py
@@ -1,10 +1,18 @@
 
-from api.views import ReviewRudView , CompanyRudView, ProductRudView
+from api.views import CompanyAPIView
+from api.views import CompanyRudView
+from api.views import ProductAPIView
+from api.views import ProductRudView
+from api.views import ReviewAPIView
+from api.views import ReviewRudView
+
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^company/(?P<pk>\d+)/$', CompanyRudView.as_view(), name='company-rud'),
     url(r'^product/(?P<pk>\d+)/$', ProductRudView.as_view(), name='product-rud'),
     url(r'^review/(?P<pk>\d+)/$', ReviewRudView.as_view(), name='review-rud'),
-
+    url(r'^company/$', CompanyAPIView.as_view(), name='company-create'),
+    url(r'^product/$', ProductAPIView.as_view(), name='product-create'),
+    url(r'^review/$', ReviewAPIView.as_view(), name='review-create'),
 ]
