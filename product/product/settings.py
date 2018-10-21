8880689858f8a39f9fa54f53ea51a98ad729b194
ahmed <ahmed.nafies@gmail.com>
2018-10-21 12:44:28 +0300

    templates path corrected in settings

--- product/product/settings.py
+++ product/product/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'product.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['product/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
